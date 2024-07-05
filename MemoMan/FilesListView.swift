b2e3358042dd6f3dffe68a30ea4dc4bb72586683
aabagdi <70984836+aabagdi@users.noreply.github.com>
2024-07-05 11:04:01 -0700

    swiftdata predicate working

---
+++ MemoMan/FilesListView.swift
@@ -0,0 +1,68 @@
+//
+//  FilesListView.swift
+//  MemoMan
+//
+//  Created by Aadit Bagdi on 7/5/24.
+//
+
+import SwiftUI
+import SwiftData
+
+struct FilesListView: View {
+    var searchString : String
+    @Query var recordings : [Recording]
+    @State private var openedGroup : UUID? = nil
+    @Environment(\.modelContext) var modelContext
+    
+    init(sort: SortDescriptor<Recording>, searchString: String) {
+        self.searchString = searchString
+        _recordings = Query(filter: #Predicate {
+            if searchString.isEmpty {
+                return true
+            }
+            else {
+                return $0.name?.localizedStandardContains(searchString) ?? false
+            }
+            
+        }, sort: [sort])
+    }
+    
+    
+    var body: some View {
+        if recordings.isEmpty {
+            Text("You have no recordings!")
+        }
+        else {
+            VStack {
+                List {
+                    ForEach(recordings.reversed(), id: \.self) { recording in
+                        PlayerView(openedGroup: $openedGroup, recording: recording)
+                            .swipeActions(edge: .trailing) {
+                                Button("Delete", systemImage: "trash", role: .destructive) {
+                                    modelContext.delete(recording)
+                                    do {
+                                        try FileManager.default.removeItem(at: recording.fileURL)
+                                    } catch {
+                                        print("Error removing item URL")
+                                    }
+                                    do {
+                                        try modelContext.save()
+                                    } catch {
+                                        print("Model Context not saving correctly")
+                                    }
+                                }
+                            }
+                            .swipeActions(edge: .leading) {
+                                ShareLink(item: recording.fileURL) {
+                                    VStack {
+                                        Image(systemName: "square.and.arrow.up")
+                                        Text("Share")
+                                    }
+                                }
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
--- MemoMan/FilesView.swift
+++ MemoMan/FilesView.swift
@@ -3,51 +3,8 @@ import SwiftData
 
 struct FilesView: View {
     @State private var searchString : String = ""
-    @State private var openedGroup : UUID? = nil
-    @Query var recordings : [Recording]
-    @Environment(\.modelContext) var modelContext
-    
-    var filtered: [Recording] {
-        guard searchString.isEmpty == false else { return recordings }
-        return recordings.filter { $0.name!.localizedStandardContains(searchString) }
-    }
-    
     var body: some View {
-        if recordings.isEmpty {
-            Text("You have no recordings!")
-        }
-        else {
-            VStack {
-                List {
-                    ForEach(filtered.reversed(), id: \.self) { recording in
-                        PlayerView(openedGroup: $openedGroup, recording: recording)
-                            .swipeActions(edge: .trailing) {
-                                Button("Delete", systemImage: "trash", role: .destructive) {
-                                    modelContext.delete(recording)
-                                    do {
-                                        try FileManager.default.removeItem(at: recording.fileURL)
-                                    } catch {
-                                        print("Error removing item URL")
-                                    }
-                                    do {
-                                        try modelContext.save()
-                                    } catch {
-                                        print("Model Context not saving correctly")
-                                    }
-                                }
-                            }
-                            .swipeActions(edge: .leading) {
-                                ShareLink(item: recording.fileURL) {
-                                    VStack {
-                                        Image(systemName: "square.and.arrow.up")
-                                        Text("Share")
-                                    }
-                                }
-                            }
-                    }
-                }
-                .searchable(text: $searchString)
-            }
-        }
+        FilesListView(sort: SortDescriptor(\Recording.name), searchString: searchString)
+            .searchable(text: $searchString)
     }
 }
