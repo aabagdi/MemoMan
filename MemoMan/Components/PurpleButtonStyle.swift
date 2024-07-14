33939688cf6865fc3957105f3cd79debd645c5c6
aabagdi <70984836+aabagdi@users.noreply.github.com>
2024-07-13 22:10:16 -0700

    initial commit

--- MemoMan/Components/PurpleButtonStyle.swift
+++ MemoMan/Components/PurpleButtonStyle.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct PurpleButtonStyle: ButtonStyle {
     func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .frame(maxWidth: .infinity)
-            .padding(EdgeInsets(top: 16, leading: 32, bottom: 16, trailing: 32))
-            .foregroundStyle(.white)
-            .background(Color("MemoManPurple"))
-            .clipShape(Capsule())
-            .scaleEffect(configuration.isPressed ? 0.90 : 1)
-            .animation(.easeInOut, value: configuration.isPressed)
+        HStack {
+            configuration.label
+                .frame(maxWidth: .infinity)
+                .padding(EdgeInsets(top: 16, leading: 32, bottom: 16, trailing: 32))
+                .foregroundStyle(.white)
+                .background(Color("MemoManPurple"))
+                .clipShape(Capsule())
+                .scaleEffect(configuration.isPressed ? 0.90 : 1)
+                .animation(.easeInOut, value: configuration.isPressed)
+        }
     }
 }
