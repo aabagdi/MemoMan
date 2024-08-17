7cd4d2861d76580fe631af8bbbbc744abd7328a5
aabagdi <70984836+aabagdi@users.noreply.github.com>
2024-08-16 22:00:13 -0700

    code cleanup

--- MemoMan/Views/PlayerView.swift
+++ MemoMan/Views/PlayerView.swift
@@ -3,22 +3,35 @@ import AVFoundation
 import SwiftData
 
 struct PlayerView: View {
-    var recording: Recording
-    @Binding var openedGroup: UUID?
-
-    @StateObject private var viewModel: PlayerViewModel
-    @State private var sliderValue: TimeInterval = 0
-
+    var recording : Recording
+    @Binding var openedGroup : UUID?
+    
+    @StateObject private var viewModel : PlayerViewModel
+    @State private var sliderValue : TimeInterval = 0
+    
     init(openedGroup: Binding<UUID?>, recording: Recording) {
         self.recording = recording
         self._openedGroup = openedGroup
         let player = Player(recording: recording)
         self._viewModel = StateObject(wrappedValue: PlayerViewModel(player: player, recording: recording))
     }
-
+    
     var body: some View {
         VStack {
-            DisclosureGroup(isExpanded: isExpandedBinding()) {
+            DisclosureGroup(isExpanded:
+                Binding(
+                    get: { self.openedGroup == self.recording.id },
+                    set: { newValue in
+                        if newValue {
+                            self.openedGroup = self.recording.id
+                        } else if self.openedGroup == self.recording.id {
+                            self.openedGroup = nil
+                            viewModel.stop()
+                            resetSlider()
+                        }
+                    }
+                )
+            ) {
                 expandedContent
             } label: {
                 Text(recording.name ?? "")
@@ -116,21 +129,6 @@ struct PlayerView: View {
         let formatter = DateComponentsFormatter()
         formatter.allowedUnits = [.hour, .minute, .second]
         formatter.zeroFormattingBehavior = .pad
-        return formatter.string(from: timeInterval) ?? "00:00"
-    }
-    
-    private func isExpandedBinding() -> Binding<Bool> {
-        Binding(
-            get: { self.openedGroup == self.recording.id },
-            set: { newValue in
-                if newValue {
-                    self.openedGroup = self.recording.id
-                } else if self.openedGroup == self.recording.id {
-                    self.openedGroup = nil
-                    viewModel.stop()
-                    resetSlider()
-                }
-            }
-        )
+        return formatter.string(from: timeInterval) ?? "00:00:00"
     }
 }
